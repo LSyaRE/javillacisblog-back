3ea6b89196cb1b4d1a64bd4516614e7585fca3ed
LSyaRE <jonasvillacis@yahoo.com>
2025-05-05 13:52:58 -0500

    initial commit

---
+++ src/main/java/com/luminesway/JavillacisblogApplication.java
@@ -0,0 +1,13 @@
+package com.luminesway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavillacisblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavillacisblogApplication.class, args);
+    }
+
+}
